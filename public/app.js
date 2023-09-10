ec952632b153b81e28da0fcc6cf92ce34d9613a5
Iamneo <techadmin@iamneo.ai>
2023-09-10 08:00:13 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,55 +1,68 @@
-const cells = document.querySelectorAll('.bt');
+const boxes = document.querySelectorAll('.bt');
+const resultContainer = document.querySelector('.result-container');
 const resultText = document.querySelector('.result-text');
 const resetButton = document.getElementById('reset-button');
+
 let currentPlayer = 'X';
 let gameBoard = ['', '', '', '', '', '', '', '', ''];
 let gameActive = true;
 
-const winningCombinations = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8],
-    [0, 3, 6], [1, 4, 7], [2, 5, 8],
-    [0, 4, 8], [2, 4, 6]
-];
+function handleBoxClick(index) {
+    if (gameBoard[index] === '' && gameActive) {
+        gameBoard[index] = currentPlayer;
+        boxes[index].value = currentPlayer;
+        checkWin();
+        togglePlayer();
+    }
+}
 
-const checkWinner = () => {
-    for (let combo of winningCombinations) {
-        const [a, b, c] = combo;
-        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
-            resultText.textContent = `Player ${gameBoard[a]} wins!`;
+function togglePlayer() {
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    resultText.textContent = `Player ${currentPlayer}'s turn`;
+}
+
+function checkWin() {
+    const winPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6]             // Diagonals
+    ];
+
+    for (const pattern of winPatterns) {
+        const [a, b, c] = pattern;
+        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[b] === gameBoard[c]) {
+            resultText.textContent = `Player ${currentPlayer} wins!`;
             gameActive = false;
             resetButton.disabled = false;
         }
     }
 
     if (!gameBoard.includes('') && gameActive) {
-        resultText.textContent = "It's a draw!";
+        resultText.textContent = 'It\'s a draw!';
+        gameActive = false;
         resetButton.disabled = false;
     }
-};
+}
 
-const handleClick = (cell, index) => {
-    if (gameBoard[index] === '' && gameActive) {
-        cell.value = currentPlayer;
-        gameBoard[index] = currentPlayer;
-        checkWinner();
-        if (gameActive) {
-            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-            resultText.textContent = `Player ${currentPlayer}'s turn`;
-        }
-    }
-};
-
-cells.forEach((cell, index) => {
-    cell.addEventListener('click', () => handleClick(cell, index));
-});
-
-resetButton.addEventListener('click', () => {
-    cells.forEach((cell, index) => {
-        cell.value = '';
-        gameBoard[index] = '';
+function resetGame() {
+    gameBoard = ['', '', '', '', '', '', '', '', ''];
+    boxes.forEach(box => {
+        box.value = '';
     });
     currentPlayer = 'X';
     resultText.textContent = `Player ${currentPlayer}'s turn`;
-    resetButton.disabled = true;
     gameActive = true;
+    resetButton.disabled = true;
+}
+
+boxes.forEach((box, index) => {
+    box.addEventListener('click', () => {
+        handleBoxClick(index);
+    });
 });
+
+resetButton.addEventListener('click', () => {
+    resetGame();
+});
+
+resetGame();
